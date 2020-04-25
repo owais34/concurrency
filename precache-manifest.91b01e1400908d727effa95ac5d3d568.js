a5df1830e5c4795274c6622d9a9873d38f915c1b
owais34 <owaishunter78@gmail.com>
2020-04-25 12:16:09 +0530

    Updates

---
+++ precache-manifest.91b01e1400908d727effa95ac5d3d568.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7592546fe98f4b43698c5f15776968dc",
+    "url": "/Live-Exchange-rate-app-/index.html"
+  },
+  {
+    "revision": "d626625bc768eb466faf",
+    "url": "/Live-Exchange-rate-app-/static/css/2.e584be57.chunk.css"
+  },
+  {
+    "revision": "2019c21da1fa794d83cd",
+    "url": "/Live-Exchange-rate-app-/static/css/main.302f0ca1.chunk.css"
+  },
+  {
+    "revision": "d626625bc768eb466faf",
+    "url": "/Live-Exchange-rate-app-/static/js/2.600365ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Live-Exchange-rate-app-/static/js/2.600365ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2019c21da1fa794d83cd",
+    "url": "/Live-Exchange-rate-app-/static/js/main.72351540.chunk.js"
+  },
+  {
+    "revision": "a4fa2655e83a558b2231",
+    "url": "/Live-Exchange-rate-app-/static/js/runtime-main.96621806.js"
+  },
+  {
+    "revision": "0b56705013376bf87c4a264eda4ed5a4",
+    "url": "/Live-Exchange-rate-app-/static/media/logo.0b567050.png"
+  }
+]);
